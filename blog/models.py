d565cb5baeda7d51d212bd30a7dc99465cbe449b
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-02-29 11:50:34 +0000

    Add urls.py to blog app

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,8 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    
     class Meta:
         ordering = ["-created_on"]
     def __str__(self):
---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+]
