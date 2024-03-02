b60c02b3081d8a8a3af53a44785cd970ae5c0055
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-03-02 13:24:21 +0000

    Add and wire up About app, fill with content as admin

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,13 @@ from .models import Post
 
 # Create your views here.
 
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
-    
+
+
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
@@ -22,14 +24,6 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
-
-
+    return render(request, "blog/post_detail.html", {"post": post},)
